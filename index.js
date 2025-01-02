9c4cd5965b9f707daed3839d980e26e59f057266
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2025-01-02 23:08:33 +0600

    create a collection and api to insert a document from client side

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app=express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port=process.env.PORT || 5000;
 
@@ -28,15 +28,37 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    //service related collection
+    const serviceCollection=client.db("carDoctor").collection("services");
+    //booking related collection
+    const carDoctorCollection =client.db("carDoctor").collection("bookings")
 
-    const carDoctorCollection=client.db("carDoctor").collection("services")
 
-    
-
+//service related api
     app.get("/services", async(req,res)=>{
-        const cursor=carDoctorCollection.find()
+        const cursor=serviceCollection.find()
         const result=await cursor.toArray()
         res.send(result)
+    }),
+
+
+    app.get("/services/:id", async(req,res)=>{
+        const id=req.params.id
+        const query = {_id: new ObjectId(id)}
+        const options = {
+            projection: {  title: 1, price: 1, service_id:1 },
+          };
+        const result=await serviceCollection.findOne(query,options)
+        res.send(result)
+    })
+
+    //bookings related api
+
+    app.post("/bookings", async(req,res)=>{
+        const bookings=req.body;
+        const result= await carDoctorCollection.find(bookings)
+        res.send(result)
+
     })
 
 
