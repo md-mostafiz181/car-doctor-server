c492dd4d29260a4661866efe6659678cf5396bd3
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2025-01-02 12:49:18 +0600

    instert multiple data from api and make a database in mongodb

--- index.js
+++ index.js
@@ -28,6 +28,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const carDoctorCollection=client.db("carDoctor").collection("services")
+
+    
+
+    app.get("/services", async(req,res)=>{
+        const cursor=carDoctorCollection.find()
+        const result=await cursor.toArray()
+        res.send(result)
+    })
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
